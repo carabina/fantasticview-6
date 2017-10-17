0e1c62835b64d0f2a9a3e702e4c269389cbf3f5a
Mobile <mobile@MacBook-Pro-Mobile.local>
2017-10-17 11:44:10 -0200

    init

---
+++ FantasticView/File.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  FantasticView
+//
+//  Created by Mobile on 17/10/17.
+//  Copyright © 2017 Mobile. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FantasticView : UIView {
+    
+    let colors: [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
+    var colorCounter = 0
+    
+    override init (frame: CGRect) {
+        super.init(frame: frame)
+        
+        let scheduledColorChanged = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { (timer) in
+            UIView.animate(withDuration: 2.0) {
+                self.layer.backgroundColor = self.colors[self.colorCounter % 6].cgColor  //3
+                self.colorCounter+=1
+            }
+        }
+        
+        scheduledColorChanged.fire()
+    }
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        
+    }
+    
+}
